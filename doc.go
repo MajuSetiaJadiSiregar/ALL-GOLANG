306e5398e230c0dffef049a06dbcd663ed592548
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-12-23 19:08:14 +0700

    cara panggil package

--- doc.go
+++ doc.go
@@ -1,7 +1,14 @@
 package dokumentasi
+
 /*
 package konsep nya hampir sama kyak yang ada di node
 cuma beda nya itu kalau di golang jika nama package sama
 maka file nya bisa diganti.
 dan penempatan nama package selalu di awal dari suatu file.
+
+package main merupakan keyword untuk menjalankan module golang.
+
+jika suatu penamaan function di buat dengan huruf kapital itu
+menandakan bahwa function tersebut bersifat public, sedangkan
+jika huruf kecil bersifat privat
 */
--- fundamentalGO/main.go
+++ fundamentalGO/main.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"AG/fundamentalGO/calculation"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hallo, Belajar Golang")
 
 	sentence := testPackage()
 
+	result := calculation.Add(8, 9)
+
 	fmt.Print(sentence)
+
+	fmt.Println(result)
 }
