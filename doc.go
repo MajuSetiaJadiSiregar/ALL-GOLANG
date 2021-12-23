b7ddd6cf49ba2a0286456c112f933ba6aae8b0e4
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-12-23 23:00:19 +0700

    add doc

--- doc.go
+++ doc.go
@@ -11,4 +11,11 @@ package main merupakan keyword untuk menjalankan module golang.
 jika suatu penamaan function di buat dengan huruf kapital itu
 menandakan bahwa function tersebut bersifat public, sedangkan
 jika huruf kecil bersifat privat
+
+di golang kita bisa mengambil package dari library golang itu sendiri.
+bisa juga mengambil package dari package yang kita buat sendiri, nah selain
+itu golang bisa ngambil package orang lain misalkan dari github
+
+func main merupakan metod utama untuk menjalankan program-program yang
+ada pada module
 */
